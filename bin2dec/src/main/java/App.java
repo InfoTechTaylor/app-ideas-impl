6fd74d2045e5a2466e82a444085fb5743dc76088
Taylor Lapointe <Taylor.Lapointe@libertymutual.com>
2019-10-23 18:56:46 -0400

    moved functionality into function

--- bin2dec/src/main/java/App.java
+++ bin2dec/src/main/java/App.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class App {
 
     public static Scanner scan = new Scanner(System.in);
-    public static void main(String[] args) {
+
+    public static void getBinaryDigits() {
         System.out.println("Enter binary digit:");
         String message = scan.nextLine();
         System.out.println(message);
+    }
+    public static void main(String[] args) {
+        getBinaryDigits();
     }
 }
