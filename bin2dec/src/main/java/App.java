e547db71b00ece7ff44a7282f9e4c46d4e5e6a0a
Taylor Lapointe <Taylor.Lapointe@libertymutual.com>
2019-10-23 19:22:11 -0400

    added input validation

--- bin2dec/src/main/java/App.java
+++ bin2dec/src/main/java/App.java
@@ -4,12 +4,22 @@ public class App {
 
     public static Scanner scan = new Scanner(System.in);
 
-    public static void getBinaryDigits() {
-        System.out.println("Enter binary digit:");
+    public static String getInput(String prompt) {
+        System.out.println(prompt);
         String message = scan.nextLine();
-        System.out.println(message);
+        return message;
     }
+
+    public static String validateInput(String input){
+        while (!input.equals("1") && !input.equals("0")){
+            input = getInput("Enter binary digits. Must be zero or one: ");
+        }
+        return input;
+    }
+
     public static void main(String[] args) {
-        getBinaryDigits();
+        String input = getInput("Enter binary digits: ");
+        input = validateInput(input);
+        System.out.println(input);
     }
 }
