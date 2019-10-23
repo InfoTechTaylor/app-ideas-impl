f40ab247a0eb9f61414fe555ec6be0887550b747
Taylor Lapointe <Taylor.Lapointe@libertymutual.com>
2019-10-23 18:48:44 -0400

    setup simple main function to get user input and print it

---
+++ bin2dec/src/main/java/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter binary digit:");
+        String message = scan.nextLine();
+        System.out.println(message);
+    }
+}
