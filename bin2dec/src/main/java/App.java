09b877b0e7fdafe5d2513a135aa52cb7304db676
Taylor Lapointe <Taylor.Lapointe@libertymutual.com>
2019-11-06 19:23:01 -0500

    removed validation and added math

--- bin2dec/src/main/java/App.java
+++ bin2dec/src/main/java/App.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class App {
 
@@ -9,15 +10,33 @@ public class App {
 
     public static String getInput(String prompt){
         String input;
-        do {
-            System.out.println(prompt);
-            input = scan.nextLine();
-        } while (!input.equals(ONE) && !input.equals(ZERO));
+
+        System.out.println(prompt);
+        input = scan.nextLine();
+
         return input;
+    }
+
+    public static int convertBinaryToDecimal(String binaryInput) {
+        int decimalResult = 0;
+        int length = binaryInput.length();
+        for(int i = length - 1; i >= 0; i--){
+            char digitChar = binaryInput.charAt(i);
+
+            int digit = Character.getNumericValue(digitChar);
+
+            if (digit == 1){
+                decimalResult += Math.pow(2,i);
+            }
+        }
+
+        return decimalResult;
     }
 
     public static void main(String[] args) {
         String input = getInput("Enter binary digits: ");
         System.out.println(input);
+        int result = convertBinaryToDecimal(input);
+        System.out.println("Your binary number of " + input + " is " + result);
     }
 }
