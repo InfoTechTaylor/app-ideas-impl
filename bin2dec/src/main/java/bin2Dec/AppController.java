6fdc6c3c893a1cc2a9f7462888ad2a15a2f26a17
taylor-lapointe <InfoTechTaylor@gmail.com>
2020-01-22 20:42:51 -0500

    setup of spring boot rest api getDecimal

--- bin2dec/src/main/java/bin2Dec/AppController.java
+++ bin2dec/src/main/java/bin2Dec/AppController.java
@@ -1,10 +1,20 @@
-package binToDec;
+package bin2Dec;
 
-// @RestController
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AppController {
 
-    // @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+	private final AtomicLong id = new AtomicLong();
+
+	@GetMapping("/getDecimal")
+	public BinaryToDecimal binaryToDecimal(@RequestParam(value = "binary", defaultValue = "0") String binary) {
+		return new BinaryToDecimal(id.incrementAndGet(), binary);
     }
+    
+    // TODO add a mapping for errors
 }
---
+++ bin2dec/src/main/java/bin2Dec/BinaryToDecimal.java
@@ -0,0 +1,62 @@
+package bin2Dec;
+
+public class BinaryToDecimal {
+    private final long id;
+    private String decimal;
+
+    public BinaryToDecimal(long id, String binary){
+        this.id = id;
+        this.decimal = convertBinaryToDecimal(binary);
+    }
+
+    public long getId() {
+		return id;
+    }
+
+    public String getDecimal() {
+        return decimal;
+    }
+
+
+    public static String convertBinaryToDecimal(String binaryInput) {
+        int decimalResult = 0;
+        int length = binaryInput.length();
+        int count = 0;
+        for(int i = length - 1; i >= 0; i--){
+            char digitChar = binaryInput.charAt(count);
+
+            int digit = Character.getNumericValue(digitChar);
+
+            if (digit == 1){
+                decimalResult += Math.pow(2,i);
+            }
+            count++;
+        }
+
+        return Integer.toString(decimalResult);
+    }
+
+    public static boolean isValidInput(String input) {
+        int length = input.length();
+        for(int i = 0; i <= length -1; i++){
+            char digitChar = input.charAt(i);
+
+            int digit = Character.getNumericValue(digitChar);
+
+            if (!isZeroOrOne(digit)){
+                return false;
+            }
+        }
+        return true;
+    };
+
+    public static boolean isZeroOrOne(int digit) {
+        if (digit != 1 && digit != 0){
+            return false;
+        }
+        return true;
+    };
+    
+
+
+}
